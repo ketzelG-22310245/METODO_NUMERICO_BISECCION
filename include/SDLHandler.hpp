61f4d0f16dfe415ee886bbd029a0cfe9a1dc8e9b
Ketzel Gibran 22310245 <A22310245@live.ceti.mx>
2023-12-05 22:36:13 -0600

    modified:   include/SDLHandler.hpp 	modified:   src/SDLHandler.cpp 	modified:   src/main.cpp

--- include/SDLHandler.hpp
+++ include/SDLHandler.hpp
@@ -3,23 +3,24 @@
 #include <SDLMusicPlayer.hpp>
 #include <SDL2/SDL.h>
 
-class SDLHandler {
+class SDLHandler
+{
 public:
     SDLHandler();
     ~SDLHandler();
 
     bool init();
-    bool loadMedia(const char* imagePath, const char* musicPath);
+    bool loadMedia(const char *imagePath, const char *musicPath);
     void close();
     void render();
     void handleEvents();
     bool shouldQuit() const;
 
 private:
-    SDL_Window* gWindow;
-    SDL_Surface* gScreenSurface;
-    SDL_Surface* gXOut;
-    SDLMusicPlayer musicPlayer;  // New member
-    Mix_Music* gMusic;
+    SDL_Window *gWindow;
+    SDL_Surface *gScreenSurface;
+    SDL_Surface *gXOut;
+    SDLMusicPlayer musicPlayer; // New member
+    Mix_Music *gMusic;
     bool quit;
 };
--- src/SDLHandler.cpp
+++ src/SDLHandler.cpp
@@ -1,27 +1,33 @@
 #include <SDLHandler.hpp>
 #include <iostream>
 
-SDLHandler::SDLHandler() : gWindow(nullptr), gScreenSurface(nullptr), gXOut(nullptr), quit(false) {
+SDLHandler::SDLHandler() : gWindow(nullptr), gScreenSurface(nullptr), gXOut(nullptr), quit(false)
+{
 }
 
-SDLHandler::~SDLHandler() {
+SDLHandler::~SDLHandler()
+{
     close();
 }
 
-bool SDLHandler::init() {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+bool SDLHandler::init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+    {
         std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
         return false;
     }
 
     gWindow = SDL_CreateWindow("Presentacion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 439, 426, SDL_WINDOW_SHOWN);
-    if (gWindow == nullptr) {
+    if (gWindow == nullptr)
+    {
         std::cerr << "Error creating window: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return false;
     }
 
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+    {
         std::cerr << "SDL_mixer could not initialize! SDL_mixer Error: " << Mix_GetError() << std::endl;
         return false;
     }
@@ -30,77 +36,93 @@ bool SDLHandler::init() {
     return true;
 }
 
-bool SDLHandler::loadMedia(const char* imagePath, const char* musicPath) {
+bool SDLHandler::loadMedia(const char *imagePath, const char *musicPath)
+{
     gXOut = SDL_LoadBMP(imagePath);
-    if (gXOut == nullptr) {
+    if (gXOut == nullptr)
+    {
         std::cerr << "Error loading image: " << SDL_GetError() << std::endl;
         return false;
     }
 
-    if (!musicPlayer.loadMusic(musicPath)) {
+    if (!musicPlayer.loadMusic(musicPath))
+    {
         return false;
     }
 
     std::cout << "Loading music from: " << musicPath << std::endl;
     gMusic = Mix_LoadMUS(musicPath);
-    if (gMusic == nullptr) {
+    if (gMusic == nullptr)
+    {
         std::cerr << "Error loading music: " << Mix_GetError() << std::endl;
         return false;
     }
 
     std::cout << "Music loaded successfully." << std::endl;
     Mix_VolumeMusic(MIX_MAX_VOLUME);
-    
+
     std::cout << "Playing music..." << std::endl;
     Mix_PlayMusic(gMusic, -1);
-    if (Mix_PlayingMusic() == 0) {
+    if (Mix_PlayingMusic() == 0)
+    {
         std::cerr << "Error playing music: " << Mix_GetError() << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "Music is playing." << std::endl;
     }
 
     return true;
 }
 
-void SDLHandler::close() {
+void SDLHandler::close()
+{
     SDL_FreeSurface(gXOut);
     SDL_DestroyWindow(gWindow);
     Mix_FreeMusic(gMusic);
     SDL_Quit();
 }
 
-void SDLHandler::render() {
+void SDLHandler::render()
+{
     SDL_BlitSurface(gXOut, nullptr, gScreenSurface, nullptr);
     SDL_UpdateWindowSurface(gWindow);
 }
 
-void SDLHandler::handleEvents() {
+void SDLHandler::handleEvents()
+{
     SDL_Event e;
-    while (SDL_PollEvent(&e) != 0) {
-        if (e.type == SDL_QUIT) {
+    while (SDL_PollEvent(&e) != 0)
+    {
+        if (e.type == SDL_QUIT)
+        {
             quit = true;
-        } else if (e.type == SDL_KEYDOWN) {
-            switch (e.key.keysym.sym) {
-                case SDLK_x:
-                    quit = true;
-                    break;
-                case SDLK_m:
-                    musicPlayer.play();
-                    break;
-                case SDLK_p:
-                    musicPlayer.pause();
-                    break;
-                case SDLK_r:
-                    musicPlayer.resume();
-                    break;
-                case SDLK_s:
-                    musicPlayer.stop();
-                    break;
+        }
+        else if (e.type == SDL_KEYDOWN)
+        {
+            switch (e.key.keysym.sym)
+            {
+            case SDLK_x:
+                quit = true;
+                break;
+            case SDLK_m:
+                musicPlayer.play();
+                break;
+            case SDLK_p:
+                musicPlayer.pause();
+                break;
+            case SDLK_r:
+                musicPlayer.resume();
+                break;
+            case SDLK_s:
+                musicPlayer.stop();
+                break;
             }
         }
     }
 }
 
-bool SDLHandler::shouldQuit() const {
+bool SDLHandler::shouldQuit() const
+{
     return quit;
 }
